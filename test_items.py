a51425d6d70120b3c4659e3d126c73b980a86a7b
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-04 19:44:03 +0500

    Add files via upload
    pytest --language=es test_items.py

---
+++ test_items.py
@@ -0,0 +1,12 @@
+
+from selenium.webdriver.common.by import By
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
+
+def test_find_add_to_card_btn(browser):
+    browser.get(link)
+    time.sleep(5)
+    assert browser.find_element(By.CSS_SELECTOR,'.btn-add-to-basket').is_displayed(), "Кнопка добавления товара в корзину отсутсвует"
+    
+
+    # pytest --language=es C:\selenium_course\test_items.py
