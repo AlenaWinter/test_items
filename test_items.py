1cf83262f97a7878a4baadd6cbaea6394ac07059
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-05 10:51:51 +0500

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -6,6 +6,8 @@ import time
 def test_find_add_to_card_btn(browser):
     browser.get(link)
     time.sleep(5)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    button.click()
     assert browser.find_element(By.CSS_SELECTOR,'.btn-add-to-basket').is_displayed(), "Кнопка добавления товара в корзину отсутсвует"
     
 
